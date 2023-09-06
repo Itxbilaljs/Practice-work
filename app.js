000267615d5fc333c6879ad4038bc6d3c2b7d9c9
itsbilaljs <mb3592038@gmail.com>
2023-09-05 20:39:16 -0700

    Pratice work

---
+++ app.js
@@ -0,0 +1,41 @@
+// const my = "aizaz"
+// const names = ['ali', 'aizaz', 'raza', 'mohammad'];
+// for (let i = 0; i < my.names; i++) {
+//     if(names[i] === my){
+//         console.log(`${my}is avaible`);
+//         break;
+//     }
+//  else {
+//         console.log(it is avaible);
+//     }
+// }
+
+// const my = "aizaz";
+// const names = ['ali', 'aizaz', 'raza', 'mohammad'];
+
+// let isAvailable = false;
+
+// for (let i = 0; i < names.length; i++) {
+//     if (names[i] === my) {
+//         isAvailable = true;
+//         console.log(`${my} is available`);
+//         break;
+//     }
+// }
+
+// if (!isAvailable) {
+//     console.log(`${my} is not available`);
+// }
+
+
+let myName = "my aizaz ali"
+let namearr = myName.slice()
+let result =[]
+for (let i = 0; i < namearr.length; i++) {
+    console.log(namearr[i].slice(0,1));
+    let firstletter = namearr[i].slice(0,1).toUpperCase();
+    let lestletter = namearr[i].slice[i].tolowerCase();
+    let comperletter = firstletter + lestletter
+    result.push()
+    
+}
